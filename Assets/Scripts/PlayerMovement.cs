dbe7bd24d585a1b8b6db8998e625284c970291aa
mzc <maplesfy@gmail.com>
2026-05-14 20:44:01 +0800

    第一次上传项目

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("移动设置")]
+    public float moveSpeed = 5f;
+    private Vector2 currentDir;
+    private Vector2 playerPos;
+    private Vector2 boxSize = new Vector2(0.8f,0.8f);
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        playerPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        //位置更新
+        Vector2 NewDir = new Vector2(h, v).normalized;
+        if (NewDir == currentDir)
+        {
+            playerPos += NewDir * moveSpeed * Time.deltaTime;
+            transform.position = playerPos;
+            return;
+        }
+        currentDir = NewDir;
+
+        //
+
+        //动画更新
+        anim.SetFloat("Speed", NewDir.magnitude);
+        if (currentDir != Vector2.zero)
+        {
+            anim.SetFloat("Horizontal", currentDir.x);
+            anim.SetFloat("Vertical", currentDir.y);
+        }
+    }
+    private void FixedUpdate()
+    {
+
+    }
+}
